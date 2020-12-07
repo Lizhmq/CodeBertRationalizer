8f41bdab90c3e88c1d75dbe357923c092901bb00
Joe Lee <lizhmq@pku.edu.cn>
2020-12-07 11:34:11 +0800

    fix tokenizer save bug

--- run_classifier.py
+++ run_classifier.py
@@ -207,7 +207,7 @@ def train(args, train_dataset, model, tokenizer, fh, pool):
                         torch.save(model_to_save.state_dict(), os.path.join(output_dir, "model.pt"))
                     else:
                         model_to_save.save_pretrained(output_dir)
-                    tokenizer.save_pretrained(output_dir)
+                    # tokenizer.save_pretrained(output_dir)
 
                     torch.save(args, os.path.join(output_dir, "training_args.bin"))
                     logger.info("Saving model checkpoint to %s", output_dir)
@@ -220,7 +220,7 @@ def train(args, train_dataset, model, tokenizer, fh, pool):
                         torch.save(model_to_save.state_dict(), os.path.join(last_output_dir, "model.pt"))
                     else:
                         model_to_save.save_pretrained(last_output_dir)
-                    tokenizer.save_pretrained(last_output_dir)
+                    # tokenizer.save_pretrained(last_output_dir)
                     idx_file = os.path.join(last_output_dir, 'idx_file.txt')
                     with open(idx_file, 'w', encoding='utf-8') as idxf:
                         idxf.write(str(0) + '\n')
