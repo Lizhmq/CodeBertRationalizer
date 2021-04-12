5277f0e1eb3c1a7c6aa85102f672da4e5dacff76
Joe Lee <lizhmq@pku.edu.cn>
2021-04-12 14:54:45 +0800

    modify evaluation script for baseline

--- baselines/eval.py
+++ baselines/eval.py
@@ -3,6 +3,7 @@ import os
 import pickle
 
 from dataset import Java, Dataset
+# from dataset2 import Py150, Dataset
 from models.lstm_cls import LSTMEncoder, LSTMClassifier
 from models.Transformer import TransformerClassifier
 
@@ -43,15 +44,15 @@ if __name__ == "__main__":
     parser.add_argument('-gpu', type=str, default='-1')
     parser.add_argument('-model', type=str, default='LSTM')
     parser.add_argument('-bs_eval', type=int, default=64)
-    parser.add_argument('-data', type=str, default='java')
-    parser.add_argument('-save_name', type=str, default='java-lstm')
+    parser.add_argument('-data', type=str, default='PY_OP')
+    parser.add_argument('-save_name', type=str, default='py-op/9.pt')
     parser.add_argument('--load_dataset', action='store_true')
     
     opt = parser.parse_args()
 
     _save = os.path.join("./save", opt.save_name)
 
-    data_dict = {"JAVA": "../../bigJava/datasets"}
+    data_dict = {"JAVA": "../../bigJava/datasets", "PY150": "../../great", "PY_OP": "../../CuBert/wrong_op"}
     _data = data_dict[opt.data.upper()]
     _load_dataset = opt.load_dataset
     _bs_eval = opt.bs_eval
@@ -59,7 +60,7 @@ if __name__ == "__main__":
     if opt.gpu == "-1":
         device = torch.device("cpu")
     else:
-        device = torch.device("cuda")
+        device = torch.device("cuda", int(opt.gpu))
         # os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu
 
     _model = opt.model
@@ -68,22 +69,28 @@ if __name__ == "__main__":
     hidden_size = 600
     n_layers = 2
     n_channel = -1
-    n_class_dict = {"JAVA": 2}
+    n_class_dict = {"JAVA": 2, "PY150": 2, "PY_OP": 2}
     n_class = n_class_dict[opt.data.upper()]
-    max_len = 400
+    max_len = {"JAVA": 400, "PY150": 250, "PY_OP": 512}[opt.data.upper()]
     bidirection = True
 
     ## WARNING: dataset is pre saved, if parameters are changed, please run dataset.py first
     if _load_dataset:
         print("Loading dataset.")
-        with open(os.path.join(_data, "Java.pkl"), "rb") as f:
+        load_path = {"JAVA": "Java.pkl", "PY150": "Py150.pkl", "PY_OP": "Python.pkl"}[opt.data.upper()]
+        with open(os.path.join(_data, load_path), "rb") as f:
             dataset = pickle.load(f)
         print("Done.")
     else:
         print("Creating dataset.")
-        dataset = Java(path=_data,
-                    max_len=max_len,
-                    vocab_size=vocab_size)
+        if opt.data.upper() in ["JAVA", "PY_OP"]:
+            dataset = Java(path=_data,
+                        max_len=max_len,
+                        vocab_size=vocab_size)
+        else:
+            dataset = Py150(path=_data,
+                        max_len=max_len,
+                        vocab_size=vocab_size)
         print("Done.")
     training_set = dataset.train
     valid_set = dataset.dev
--- baselines/eval_rationale.py
+++ baselines/eval_rationale.py
@@ -9,7 +9,7 @@ from rationale.contiguous import ContiguousThresholder
 from rationale.topk import TopKThresholder
 from rationale.evaluator import Evaluator
 from dataset import Dataset, Java
-
+from utils import set_seed
 
 def load_data(path):
     with open(path, "rb") as f:
@@ -17,8 +17,10 @@ def load_data(path):
     return data
 
 def main():
+    set_seed(2333)
     device = torch.device("cuda", 0)
     model_path = "./save/lstm/11.pt"
+    # model_path = "./save/py-op/9.pt"
     vocab_size = 30000
     embedding_size = 512
     hidden_size = 600
@@ -26,7 +28,8 @@ def main():
     n_channel = -1
     n_class_dict = {"JAVA": 2}
     n_class = n_class_dict["JAVA"]
-    max_len = 400
+    # max_len = 400
+    max_len = 512
     bidirection = True
 
     enc = LSTMEncoder(embedding_dim=embedding_size, hidden_dim=hidden_size,
@@ -37,21 +40,50 @@ def main():
     classifier.eval()
     # classifier = myDataParallel(classifier).to(device)
 
-    scorer1 = SaliencyScorer(classifier)
-    extractor1 = TopKThresholder(0.01)
-    extractor2 = ContiguousThresholder(0.01)
-
     data_path = "../../bigJava/datasets/test_tp_lstm.pkl"
-    
+    # data_path = '../../CuBert/wrong_op/test_tp_lstm.pkl'
+
     data = load_data(data_path)
 
-    print("Attention - TopK:")
-    evaluator = Evaluator(scorer1, extractor1, data)
-    print(evaluator.evaluate())
 
-    print("Attention - Contiguous:")
-    evaluator = Evaluator(scorer1, extractor2, data)
-    print(evaluator.evaluate())
+    scorer1 = SaliencyScorer(classifier)
+    extractor1 = TopKThresholder(1)
+    # extractor2 = ContiguousThresholder(0.0001)
+
+
+    for i in range(5):
+        print("Attention - TopK:")
+        evaluator = Evaluator(scorer1, extractor1, data)
+        evaluator.shuffle()
+        extractor1._max_length_ratio = 1
+        print(evaluator.evaluate())
+
+        extractor1._max_length_ratio = 2
+        print(evaluator.evaluate())
+
+        extractor1._max_length_ratio = 3
+        print(evaluator.evaluate())
+        
+        extractor1._max_length_ratio = 4
+        print(evaluator.evaluate())
+
+        extractor1._max_length_ratio = 5
+        print(evaluator.evaluate())
+
+        extractor1._max_length_ratio = 6
+        print(evaluator.evaluate())
+
+        extractor1._max_length_ratio = 7
+        print(evaluator.evaluate())
+
+        extractor1._max_length_ratio = 8
+        print(evaluator.evaluate())
+
+
+
+        # print("Attention - Contiguous:")
+        # evaluator = Evaluator(scorer1, extractor2, data)
+        # print(evaluator.evaluate())
 
 
 
